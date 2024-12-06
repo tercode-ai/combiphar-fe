f2d95a266622819a3c8c92ea87d51d15e207afb1
Yoga Pratama <yogapr.dev@gmail.com>
2024-12-06 15:36:45 +0700

    fix: mobile sidebar item

--- src/components/shared/mobile-sidebar.tsx
+++ src/components/shared/mobile-sidebar.tsx
@@ -1,9 +1,10 @@
 import DashboardNav from '@/components/shared/dashboard-nav';
 import { Sheet, SheetContent } from '@/components/ui/sheet';
-import { adminItems } from '@/constants/data';
+import { adminItems, userItems } from '@/constants/data';
 import { Dispatch, SetStateAction } from 'react';
 import { Link } from 'react-router-dom';
 import { LogoutButton } from '@/pages/auth/logout';
+import { useAuth } from '@/providers/auth-provider';
 
 type TMobileSidebarProps = {
   className?: string;
@@ -14,6 +15,10 @@ export default function MobileSidebar({
   setSidebarOpen,
   sidebarOpen
 }: TMobileSidebarProps) {
+  const { role } = useAuth();
+
+  const navItems = role === 'admin' ? adminItems : userItems;
+
   return (
     <Sheet open={sidebarOpen} onOpenChange={setSidebarOpen}>
       <SheetContent
@@ -26,7 +31,7 @@ export default function MobileSidebar({
               Combiphar POC LLM
             </Link>
             <div className="space-y-1 px-2">
-              <DashboardNav items={adminItems} setOpen={setSidebarOpen} />
+              <DashboardNav items={navItems} setOpen={setSidebarOpen} />
             </div>
           </div>
         </div>
